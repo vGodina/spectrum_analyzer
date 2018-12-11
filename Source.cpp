29ff71c21d9b474a1bcfc82871743f0996755124
vGodina <vladimir.godyna@gmail.com>
2018-12-11 17:50:42 +0300

    encapsulation of global objects

--- Source.cpp
+++ Source.cpp
@@ -1,37 +1,59 @@
-#include "fltk/Fl.h"
-#include "fltk/Fl_Box.h"
-#include "fltk/Fl_Window.H"
-#include "fltk/Fl_Button.h"
-#include "fltk/Fl_Native_File_Chooser.h"
-#include "audiofile/AudioFile.h"
+#include <fltk/Fl.h>
+#include <fltk/Fl_Box.h>
+#include <fltk/Fl_Window.H>
+#include <fltk/Fl_Button.h>
+#include <fltk/Fl_Native_File_Chooser.h>
+#include <audiofile/AudioFile.h>
 #include <string>
 
-Fl_Box box(10, 10, 200, 200, "");
-AudioFile <float> audioFile;
-
-void whenPushed(Fl_Widget* but, void*)
+class myWindow : public Fl_Window
 {
-	// Configuring File Chooser widget
-	Fl_Native_File_Chooser* chooser = new Fl_Native_File_Chooser();
-	chooser->type(Fl_Native_File_Chooser::BROWSE_FILE);
-	chooser->title("Open a file");
-	chooser->filter("*.wav");
-	chooser->show();
-	// getting audiofile path from File Chooser to AudioFile object
-	audioFile.load(chooser->filename());
-	// printing audiofile duration
-	std::string fileDuration = std::to_string(audioFile.getLengthInSeconds());
-	std::string labelMessage = "File duration: " + fileDuration + " seconds";
-	box.copy_label(labelMessage.c_str());
-	box.redraw();
-}
+public:
+	myWindow(int w, int h, const char* title);
+	~myWindow();
+	Fl_Button* open_button;
+	Fl_Native_File_Chooser* chooser;
+	Fl_Box* box;
+	AudioFile <float> audioFile;
+
+private:
+	static void cb_open_button(Fl_Widget*, void*);
+	inline void chooser_show();
+};
 
 int main()
 {
-	Fl_Window* window = new Fl_Window(400, 200, "Check audio file duration");
-	Fl_Button* open_button = new Fl_Button(110, 10, 100, 20, "open .wav file");
-	window->add(box);
-	open_button->callback(whenPushed);
-	window->show();
+	myWindow win(300, 200, "Spectrum Analyzer");
 	return Fl::run();
+}
+
+myWindow::myWindow(int w, int h, const char* title) :Fl_Window(w, h, title)
+{
+	begin();
+	open_button = new Fl_Button(100, 30, 100, 30, "Open .wav file");
+	chooser = new Fl_Native_File_Chooser();
+	chooser->title("Open a file");
+	chooser->filter("*.wav");
+	open_button->callback(cb_open_button, this);
+	box = new Fl_Box(40, 100, 220, 30, "000");
+	end();
+	resizable(this);
+	show();
+}
+//----------------------------------------------------
+myWindow::~myWindow() {}
+
+void myWindow::cb_open_button(Fl_Widget* openButton, void* obj)
+{
+	((myWindow*)obj)->chooser_show();
+}
+
+void myWindow::chooser_show()
+{
+	chooser->show();
+	audioFile.load(chooser->filename());
+	std::string fileDuration = std::to_string(audioFile.getLengthInSeconds());
+	std::string labelMessage = "File duration: " + fileDuration + " seconds";
+	box->copy_label(labelMessage.c_str());
+	box->redraw();
 }
