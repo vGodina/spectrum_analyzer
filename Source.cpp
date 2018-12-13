6e0f94999ff4274ee415de1277dd5bee08623254
vGodina <vladimir.godyna@gmail.com>
2018-12-14 02:21:52 +0300

    moving include list to MainWindow.h

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,3 @@
-#include <fltk/Fl.h>
-#include <fltk/Fl_Box.h>
-#include <fltk/Fl_Window.H>
-#include <fltk/Fl_Button.h>
-#include <fltk/Fl_Native_File_Chooser.h>
-#include <audiofile/AudioFile.h>
-#include <string>
 #include <MainWindow.h>
 
 int main()
--- external/include/MainWindow.h
+++ external/include/MainWindow.h
@@ -1,3 +1,11 @@
+#include <fltk/Fl.h>
+#include <fltk/Fl_Box.h>
+#include <fltk/Fl_Window.H>
+#include <fltk/Fl_Button.h>
+#include <fltk/Fl_Native_File_Chooser.h>
+#include <audiofile/AudioFile.h>
+#include <string>
+
 class MainWindow : public Fl_Window
 {
 public:
